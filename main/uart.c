8fa80111232b3fe948d81d4730c2081942d27c42
vladichos <137454914+vladichos@users.noreply.github.com>
2023-06-28 21:13:03 -0500

    Update uart.c

--- main/uart.c
+++ main/uart.c
@@ -4,8 +4,8 @@ void EstadoSemaforo(enum estados actual){
     static enum estados pasado;
 
     if (pasado != actual){
-        Serial.print("Cambio en semaforo: Estado Actual: ");
-        Serial.println(actual);
+//        Serial.print("Cambio en semaforo: Estado Actual: ");
+//        Serial.println(actual);
     }
     pasado = actual;
 }
